55b177b69b64f9c39ad544b318e5652ab6456afc
C Park <changyeonclarapark@Cs-MacBook-Air.local>
2017-09-09 00:07:42 -0700

    edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,6 @@
 $(document).ready(function() {
-
-  // $('h2').animate({left:200, opacity:"show"}, 3500); //shifts page divs
-  // $('h2').delay(1200).fadeOut(function() {
-  //   $(this).text("...is").show();
-    // $(this).delay(2400).text("...is cool").show(); //how to toggle?
-    // $(this).text("...is sweet").show();
-    // $(this).text("...is weird").show();
-  // })
-
-  // $("h2").delay(1000).animate({'padding-left': '50%'}, {duration:1000}); this moves all other divs too
-  // $('#scroll').effect( "bounce", {arguments}, speed ); //why doesn't this work
   $('#scroll-img').click(function() {
-    $('html, body').scrollTop(700); //make this smoother with animate?
+    $('html, body').scrollTop(700); 
   })
 
   $('#button').click(function() {
@@ -24,18 +13,19 @@ $(document).ready(function() {
     $('#modal-box').fadeOut(400, function() {
       $('#modal-overlay').fadeOut(400);
     });
-    $('body').removeClass('no-scroll'); //delay doesn't work to delay the removing of no scroll - 
-                                        //you can scroll even when the fadeout is still happening
+    $('body').removeClass('no-scroll'); 
   })
 
   $('.flavor').hover(function() {
     $(this).children('.flavor-img').fadeTo(400, 0.1);
     $(this).children('.name').fadeIn(500);
+    $(this).find('.yelp').fadeIn(500);
   }, 
   function() {
     $(this).stop();
     $(this).children('.flavor-img').fadeTo(400, 1);
     $(this).children('.name').hide();
+    $(this).find('.yelp').hide();
   })
 
   var divs = $('div[id^="subtext"]').hide(),
@@ -54,14 +44,12 @@ $(document).ready(function() {
 
   $(document).scroll(function () {
     var y = $(this).scrollTop();
-    if (y > 1600) {
+
+    if (y > 1800) {
         $('svg').fadeIn();
         $('#fact').delay(2100).fadeIn(800);
         $('#fact-img').delay(2100).fadeIn(800);
-    } else {
-        $('svg').fadeOut();
-        $('#fact').hide();
-        $('#fact-img').hide();
+        $('hr').delay(2200).fadeIn(800);
     }
 
 });
